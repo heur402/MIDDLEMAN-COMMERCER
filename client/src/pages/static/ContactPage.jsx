4235b1fc428853ebed6cd3152405cc880bd5a83c
BYIRINGIRO BONHEUR <byiringirobon01fra@gmail.com>
2026-07-18 13:24:21 +0200

    feat: scaffold ContactPage with page wrapper

---
+++ client/src/pages/static/ContactPage.jsx
@@ -0,0 +1,12 @@
+import PageWrapper from '../../components/layout/PageWrapper'
+
+export default function ContactPage() {
+  return (
+    <PageWrapper>
+      <div className="max-w-3xl mx-auto px-4 py-12">
+        <h1 className="text-3xl font-black text-gray-900 mb-2">Contact Us</h1>
+        <p className="text-gray-500">We'd love to hear from you.</p>
+      </div>
+    </PageWrapper>
+  )
+}
